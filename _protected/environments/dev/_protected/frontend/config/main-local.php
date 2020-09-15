f9e86d10169997669c2012f719f8b8dc93fe1d9d
Ahmed Faisal <ah.faisal@gmail.com>
2020-09-15 15:41:45 +0500

    gii server run settings update

--- _protected/environments/dev/_protected/frontend/config/main-local.php
+++ _protected/environments/dev/_protected/frontend/config/main-local.php
@@ -12,10 +12,16 @@ $config = [
 if (!YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        'allowedIPs' => ['*']   // required is on a remote server from dev machine
+    ];
 
     $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs' => ['*']  // required is on a remote server from dev machine
+    ];
 }
 
 return $config;
