7ce10ed1e4632d809578463c11668065f46b1d38
Ahmed Faisal <ah.faisal@gmail.com>
2021-06-21 18:41:31 +0500

    added some app file based transalations for generic informaiton as well as inserted dv language test to Frontend/SiteController

---
+++ _protected/common/translations/dv/app.php
@@ -0,0 +1,125 @@
+<?php
+
+return [
+
+    //-- general buttons --//
+    'Submit' => 'ހުށަހަޅާ',
+    'Cancel' => 'ކެންސަލް',
+    'Update' => 'ބަދަލްކުރުމައް',
+    'Delete' => 'ފޮހެލާ',
+    'Create' => 'ހަދާ',
+    'Send'   => 'ފޮނުވާ',
+    'Save'   => 'ލިޔެލާ',
+    'Menu'   => 'މެނޫ',
+    'Back'   => 'ފަހަތައް',
+
+    //-- CMS buttons --//
+    'Create User' => 'ޔޫސާރ ހަދާ',
+    'Create Article' => 'އާރޓިކަލް ހަދާ',
+
+    //-- top menu --//
+    'Home'     => 'ފުރަތަމަ ސަފްހާ',
+    'About'    => 'އާއިމެދު',
+    'Contact'  => 'ގުޅާ',
+    'Login'    => 'ވަދޭ',
+    'Logout'   => 'ނުކުތުން',
+    'Users'    => 'ޔޫސަރ',
+    'Articles' => 'އާރޓިކަލް',
+    'Register' => 'ރެޖިސްޓަރ',
+
+    //-- static pages --//
+    'Password' => 'ޕާސްވޯރޑް',
+    'Username' => 'ބެނުމީހާ ނަން',
+    
+    // contact
+    'Name'    => 'ނަން',
+    'Subject' => 'މައުލޫ',
+    'Text'    => 'ލިޔުން',
+    'Verification Code' => 'ޔަގީންކުރާ ކޮޑު',
+    'If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.' 
+        => 'އެއްވެސް އިތުރު މައުލޫމާތެއް ބޭނުންވާނަމަ ، ގުޅުން ފޯރމު ފުޜާލާފަ ފޮނުވާލައްވާ',
+    'Thank you for contacting us. We will respond to you as soon as possible.' => 'ގުޅާލީތީ ޝުކުރިއްޔާ، ވީއެއްމެ އަވަހަކަށް ތިކަމާ އެބައުޅެން',
+    'There was an error sending email.' => 'އީމެއިލް ފޮނުވުމުގަ މައްސަލައެއް ދިމާވެއްޖެ',
+
+    // password reset
+    'If you forgot your password you can' => 'ސިއްރުބަސް ހަދާންނެތިއްޖެޔާ',
+    'reset it' => 'ބަދަލް ކޮށްލާ',
+    'Request password reset' => 'ސިއްރުބަސް ބަދަލް ކުރުމަށް ހުށަހަޅާ',
+    'Please fill out your email.' => 'އީމެއިލް ފުރާލާ',
+    'Reset password' => 'ސިއްރުބަސް ބަދަލްކޮށްލާ',
+    'A link to reset password will be sent to your email.' => 'ކަލޭގެ އީމެއިލްއަށް ސިއްރުބަސް ބަދަލް ކުރު ލިންކެއް ފޮނުވޭނެ',
+    'Check your email for further instructions.' => 'އިތުރު ތަފްސީލު ބެލުމަށް އީމެއިލް ބަލާލާ',
+    'Please choose your new password:' => 'އާ ސިއްރުބަސް ޖައްސަވާ',
+    'New password was saved.' => 'އާ ސިއްރު ބަސް ލިޔެވިއްޖެ',
+    'Sorry, we are unable to reset password for email provided.' => 'މައާފަށް އެދެން. ތި އީމޭލްގެ ސިއްރުބަސް ބަދަލެއް ނުވި',
+    'Password reset token cannot be blank.' => 'ސިއްރުބަސް ބަދަލްކުރާ ނަންބަރ ހުސްކޮނެއް ނުބޭންދޭނެ',
+    'Wrong password reset token.' => 'ނުބައި ސިއޮރުބަސް ބަދަލް ކުރާ ނަންބަރުއް',
+
+    // signup
+    'Signup'   => 'ބައިވެރިވޭ',
+    'Please fill out the following fields to signup:' => 'ބައިވެރިވުމައް ޖެހިގެން އަންނަ ބައިތައް ފުރާލަދެއްވާ',
+    'We will send you an email with account activation link.' => 'އެކައުންޓް ޔަގީންކުރުމަށް އަޅުގަނޑުމެން އީމޭލް އެއް ފޮނުވާލައިފިން',
+    'We couldn\'t sign you up, please contact us.' => 'ބައިވެރިވުމުގައި މައްސަލައެއް ދިމާ ވެއްޖެ، އަޅުގަނޑުމެނާ ގުޅުއްވާ',
+
+    // login
+    'Remember me'  => 'މަ ހަދާން ކުރޭ',
+    'Please fill out the following fields to login:' => 'ވަނުމަށް ޖެހިގެން އޮއް ބައިތައް ފުރާލާ',
+    'You have to activate your account first. Please check your email.' => 'އެކައުންޓް އެކްޓިވޭޓް ކޮއްލައްވާ. އީމޭލްގަ ބައްލަވާލައްވާ',
+    'To be able to log in, you need to confirm your registration. Please check your email, we have sent you a message.'
+        => 'ލޮގިން ކުރުމަށް ރެޖިސްޓްރޭޝަން ކޮންފާރމް ކޮއްލައްވަން ޖެހޭނެ. ތި ބޭފުޅާގެ އީމޭލް ބައްލަވާ ލައްވާ. އަޅުގަނޑުމެ އީމޭލްއެއް ފޮނުވައި ލާފިން',
+
+    // account activation
+    'We couldn\'t send you account activation email, please contact us.' => 'އެކައުންޓް އެކްޓިވޭޓުކުރާ އީމޭލްއެއް ނުފޮނުވުނު. އަޅުގަނޑުމެނާ ގުޅުއްވާ',
+    'Success! You can now log in.' => 'ކާމިޔާބް، މިހާރު ވަދެވޭނެ',
+    'for joining us!' => 'ބައިވެރިވީތީ',
+    'your account could not be activated, please contact us!' => 'ތި އެކައުންޓެއް އެކްޓިވޭޓެއްނުކުރެވުނު. އަޅުގަނޑުމެންނާ ގުޅުއްވާ',
+    'Account activation token cannot be blank.' => 'އެކައުންޓް އެކްޓިވޭޓު ކުރާ ޓޯކަން ނަންބަރު ހުސްކޮއް ކުރިޔަކަށް ނުދާނެ',
+    'Wrong account activation token.' => 'ނުބައި އެކައުންޓް އެކްޓިވޭޝަން ޓޯކަން ނަންބަރެއް',
+
+    //-- general db fields --//
+    'Created At' => 'ހެދި ތާރީހް',
+    'Updated At' => 'ބަދަލްކުރި ތާރީހް',
+
+    //-- mixed --//
+    'My Company' => 'މަގޭ ވިޔަފާރި',
+    'Hello' => 'އައްސަލާމް އަލައެކުމް',
+    'Thank you' => 'ޝުކުރިއްޔާ',
+    
+    //-- users management --//
+    'Role' => 'މަގާމް',
+    'Create User' => 'ބަވެރިޔާ ހަދާ',
+    'Update User' => 'ބައިވެރިޔާ ބަދަލްގެނައުން',
+    'New pwd ( if you want to change it )' => 'އާ ސިއްރުބަސް ( ބަދަލް ކުރަން ބޭނުންނަމަ )',
+
+    //-- articles --//
+    'Title'    => 'ސުރުހީ',
+    'Summary'  => 'ހުލާސާ',
+    'Content'  => 'މައިގަނޑު',
+    'Status'   => 'ސްޓޭޓަސް',
+    'Category' => 'ކެޓެގޮރީ',
+    'Author'   => 'ލިޔުންތެރިޔާ',
+    'articles' => 'ލޮޔުން',
+    'news'     => 'ހަބަރު',
+    'The best news available' => 'ހުރި އެންމެ ރަގަޅު ލިޔުންތައް',
+    'We haven\'t created any articles yet.' => 'އަޅުގަނޑުމެން އަދި އެއްވެސް ލިޔުމެޢް ނުލަން',
+
+    // statuses
+    'Draft'     => 'ޑްރާފްޓް',
+    'Published' => 'ޝާއިރުކުރެވިފައި',
+    'Archived'  => 'ރައްކާކުރެވިފައި',
+
+    // categories
+    'Economy' => 'އިގްތިސާދު',
+    'Society' => 'މުޖްތަމައު',
+    'Sport'   => 'ކުޅިވަރު',
+
+    //-- errors --//
+    'The above error occurred while the Web server was processing your request.' => 'ތި އެދުން ފުރިހަމަކުރުމުގެ ތެރޭގައި މަތީގައިވާ ވެބް ސާރވަރގެ އެރަރޔާ ދިމާވެއްޖް',
+    'Please contact us if you think this is a server error. Thank you.' => 'މީ ސާރވަރ އެރާއެއް ކަމަށް ލަފާކުރެވެންޏާ އަޅުގަނޑުމެންނާ ގުލުއްވާ',
+    'You are not allowed to access this page.' => 'މި ސަފްހާ އައް ވަނުމުގެ ހުއްދައެއް ނެތް',
+
+    //-- delete messages --//
+    'Are you sure you want to delete this user?' => 'މި ބޭނުންވެރިޔާ ފޮހެލަންވީތޯ؟',
+    'Are you sure you want to delete this article?' => 'މި ލިޔުން ފޮހެލަންވީތޯ ؟',
+
+];
